e96190e6029b716021a2d24812b9cf4704afba6f
harichselvam <90306857+harichselvamc@users.noreply.github.com>
2024-07-08 14:05:35 +0530

    Update complexaddition.cs

--- complexaddition.cs
+++ complexaddition.cs
@@ -6,24 +6,33 @@ namespace ComplexNumberAddition
     {
         static void Main(string[] args)
         {
-            // Read the first complex number
-            Console.Write("Enter the real part of the first complex number: ");
-            double real1 = double.Parse(Console.ReadLine());
-            Console.Write("Enter the imaginary part of the first complex number: ");
-            double imag1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the first complex number: ");
+            string complex1 = Console.ReadLine();
+            Console.Write("Enter the second complex number: ");
+            string complex2 = Console.ReadLine();
+            (double real1, double imag1) = ParseComplexNumber(complex1);
+            (double real2, double imag2) = ParseComplexNumber(complex2);
 
-            // Read the second complex number
-            Console.Write("Enter the real part of the second complex number: ");
-            double real2 = double.Parse(Console.ReadLine());
-            Console.Write("Enter the imaginary part of the second complex number: ");
-            double imag2 = double.Parse(Console.ReadLine());
-
-            // Add the complex numbers
             double realSum = real1 + real2;
             double imagSum = imag1 + imag2;
-
-            // Display the result
             Console.WriteLine("The sum of the complex numbers is: " + realSum + " + " + imagSum + "i");
+        }
+
+        static (double, double) ParseComplexNumber(string complex)
+        {
+            
+            complex = complex.Replace(" ", "");
+
+         
+            int signIndex = complex.IndexOf('+', 1);
+            if (signIndex == -1) signIndex = complex.LastIndexOf('-', 1); 
+
+            string realPart = complex.Substring(0, signIndex);
+            string imagPart = complex.Substring(signIndex).Replace("i", "");
+            double real = double.Parse(realPart);
+            double imag = double.Parse(imagPart);
+
+            return (real, imag);
         }
     }
 }
